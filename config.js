ccdf6ce29a30b4f481ea4ce3480e3f8f3c6bc731
Damodar Dahal <underscoredam@gmail.com>
2015-12-09 10:09:37 +0545

    updated colors

--- config.js
+++ config.js
@@ -34,6 +34,6 @@ module.exports={
     port:10003,
 
 
-    routeColors:["#4183D7", "#03C9A9", "#FDE3A7", "#F9690E", "#F9BF3B", "#2980b9", "#f1c40f", "#34495e"]
+    routeColors:["#4183D7", "#03C9A9", "#F9690E", "#F9BF3B", "#2980b9", "#f1c40f", "#34495e"]
 
 }
